0968b08ea1408ddefad30981e83131401d61b0e3
shira <shira7867@gmail.com>
2018-07-02 02:39:55 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, abort, make_response
+from convert import convert_img
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html', title='yakudo')
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    try:
+        input_data = request.files['file'].read()
+        output_data = convert_img(input_data)
+        res = make_response()
+        res.data = output_data
+        res.headers['Content-Disposition'] = 'inline;'
+        res.headers['Content-Type'] = 'image/png'
+        return res
+    except:
+        abort(500)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ convert.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+
+
+def convert_img(file_data):
+    nparr = np.fromstring(file_data, np.uint8)
+    orig = cv2.imdecode(nparr, cv2.IMREAD_COLOR).astype(np.float32)
+    height, width, _ = orig.shape
+    center_x, center_y = height/2, width/2
+    blur, iterations = 0.007, 25
+
+    map_x1 = np.fromfunction(
+        lambda x, y: x + (x - center_x) * blur, (height, width),
+        dtype=np.float32)
+    map_y1 = np.fromfunction(
+        lambda x, y: y + (y - center_y) * blur, (height, width),
+        dtype=np.float32)
+    map_x2 = np.fromfunction(
+        lambda x, y: x - (x - center_x) * blur, (height, width),
+        dtype=np.float32)
+    map_y2 = np.fromfunction(
+        lambda x, y: y - (y - center_y) * blur, (height, width),
+        dtype=np.float32)
+
+    dst = np.copy(orig)
+    for i in range(iterations):
+        enlarged = cv2.remap(dst, map_y1, map_x1, cv2.INTER_LINEAR)
+        shrinked = cv2.remap(dst, map_y2, map_x2, cv2.INTER_LINEAR)
+        dst = cv2.addWeighted(enlarged, 0.5, shrinked, 0.5, 0, dst)
+
+    _, buf = cv2.imencode('.png', dst)
+    return buf.tobytes()
