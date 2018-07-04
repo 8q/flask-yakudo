2972391e76acd8c4b18d551e4e8bfcc75236ee6f
shirasu <shira7867@gmail.com>
2018-07-04 19:49:27 +0900

    modified

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from yakudo_error import YakudoError
+import random
 
 
 def convert_img(file_data):
@@ -11,19 +12,23 @@ def convert_img(file_data):
     if width > 1500 or height > 1500:
         raise YakudoError("File size must be under 1500x1500.")
     center_x, center_y = height/2, width/2
-    blur, iterations = 0.0085, 20
+    # blur, iterations = 0.0085, 20
+    iterations = 20
+
+    def blur():
+        return random.uniform(0.005, 0.010)
 
     map_x1 = np.fromfunction(
-        lambda x, y: x + (x - center_x) * blur, (height, width),
+        lambda x, y: x + (x - center_x) * blur(), (height, width),
         dtype=np.float32)
     map_y1 = np.fromfunction(
-        lambda x, y: y + (y - center_y) * blur, (height, width),
+        lambda x, y: y + (y - center_y) * blur(), (height, width),
         dtype=np.float32)
     map_x2 = np.fromfunction(
-        lambda x, y: x - (x - center_x) * blur, (height, width),
+        lambda x, y: x - (x - center_x) * blur(), (height, width),
         dtype=np.float32)
     map_y2 = np.fromfunction(
-        lambda x, y: y - (y - center_y) * blur, (height, width),
+        lambda x, y: y - (y - center_y) * blur(), (height, width),
         dtype=np.float32)
 
     for i in range(iterations):
