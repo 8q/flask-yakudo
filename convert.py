aac8d7efc6d67ddfc97ce60eb9d80f2c52ca97d4
shirasu <shira7867@gmail.com>
2018-07-30 08:27:32 +0900

    画像変換方法を変更

--- convert.py
+++ convert.py
@@ -9,8 +9,8 @@ def convert_img(file_data):
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR).astype(np.float32)
     del nparr
     height, width, _ = img.shape
-    if width > 1500 or height > 1500:
-        raise YakudoError("File size must be under 1500x1500.")
+    if width > 1000 or height > 1000:
+        raise YakudoError("File size must be under 1000x1000.")
     center_x, center_y = height/2, width/2
     # blur, iterations = 0.0085, 20
     iterations = 20
@@ -19,22 +19,22 @@ def convert_img(file_data):
         return random.uniform(0.005, 0.010)
 
     map_x1 = np.fromfunction(
-        lambda x, y: x + (x - center_x) * blur(), (height, width),
-        dtype=np.float32)
+        lambda x, y: np.vectorize(lambda x: np.float32(max(min(x + (x - center_x) * blur(), height-1), 0)))(x),
+        (height, width), dtype=np.float32)
     map_y1 = np.fromfunction(
-        lambda x, y: y + (y - center_y) * blur(), (height, width),
-        dtype=np.float32)
+        lambda x, y: np.vectorize(lambda y: np.float32(max(min(y + (y - center_y) * blur(), width-1), 0)))(y),
+        (height, width), dtype=np.float32)
     map_x2 = np.fromfunction(
-        lambda x, y: x - (x - center_x) * blur(), (height, width),
-        dtype=np.float32)
+        lambda x, y: np.vectorize(lambda x: np.float32(max(min(x - (x - center_x) * blur(), height-1), 0)))(x),
+        (height, width), dtype=np.float32)
     map_y2 = np.fromfunction(
-        lambda x, y: y - (y - center_y) * blur(), (height, width),
-        dtype=np.float32)
+        lambda x, y: np.vectorize(lambda y: np.float32(max(min(y - (y - center_y) * blur(), width-1), 0)))(y),
+        (height, width), dtype=np.float32)
 
     for i in range(iterations):
         enlarged = cv2.remap(img, map_y1, map_x1, cv2.INTER_LINEAR)
         shrinked = cv2.remap(img, map_y2, map_x2, cv2.INTER_LINEAR)
         img = cv2.addWeighted(enlarged, 0.5, shrinked, 0.5, 0, img)
 
-    _, buf = cv2.imencode('.jpg', img[15:height-15, 15:width-15])
+    _, buf = cv2.imencode('.jpg', img)
     return buf.tobytes()
